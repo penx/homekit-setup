05aadb5a2503f6260d16e86861dd6958b9141a3a
penx <penx@users.noreply.github.com>
2015-08-04 19:57:01 +0100

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,7 +1,9 @@
 #prereqs before running: minimual ubuntu-standard 14.04 installation
 
 #install apt prereqs
-sudo apt-get update
+stat -c %y /var/lib/apt/periodic/update-success-stamp
+#TODO: run update if above timestamp is old or doesn't exist
+#sudo apt-get update
 sudo apt-get install --yes git curl make python g++ libavahi-compat-libdnssd-dev build-essential
 #node
 if !(hash npm 2>/dev/null); then
@@ -9,7 +11,7 @@ if !(hash npm 2>/dev/null); then
   sudo apt-get install --yes nodejs
 fi
 #install homebridge
-if [ ! -d "homebridge" ]; then; then
+if [ ! -d "homebridge" ]; then
   git clone https://github.com/nfarina/homebridge.git
   cd homebridge
   npm install
