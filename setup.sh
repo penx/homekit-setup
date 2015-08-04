95db9adc95431e50f68c57eab2b72781d7b594cd
Ali <i_have@mac.com>
2015-08-04 16:59:19 +0000

    initial install

--- setup.sh
+++ setup.sh
@@ -1,4 +1,6 @@
 #prereqs- fresh ubuntu 12.04 image, no ui
 #install Node
+sudo apt-get install --yes curl
 curl --silent --location https://deb.nodesource.com/setup_0.12 | sudo bash -
 sudo apt-get install --yes nodejs
+npm install homebridge
