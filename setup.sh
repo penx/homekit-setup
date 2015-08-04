179e849253e5cbcc13ef311d95e518ba53fcfaf4
Ali <i_have@mac.com>
2015-08-04 18:48:34 +0000

    updated to copy config run homebridge

--- setup.sh
+++ setup.sh
@@ -1,7 +1,7 @@
 #prereqs- minimual ubuntu-standard 14.04 installation
 #sudo apt-get update
 #curl
-sudo apt-get install --yes git curl make python g++ libavahi-compat-libdnssd-dev
+sudo apt-get install --yes git curl make python g++ libavahi-compat-libdnssd-dev build-essential
 
 #node
 if !(hash npm 2>/dev/null); then
@@ -13,4 +13,8 @@ if !(hash homebridge 2>/dev/null); then
   git clone https://github.com/nfarina/homebridge.git
   cd homebridge
   npm install
+  cd ..
 fi
+cp config.json homebridge/
+cd homebridge
+npm run start
