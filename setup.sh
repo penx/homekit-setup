3790f9f3e4c5ce675ca2a6f1dabadad815c9bdf4
Ali <i_have@mac.com>
2015-08-04 17:24:38 +0000

    install make and other prereqs

--- setup.sh
+++ setup.sh
@@ -1,8 +1,23 @@
 #prereqs- fresh ubuntu 12.04 image, no ui
-sudo apt-get update
+#sudo apt-get update
+#curl
+if !(hash curl 2>/dev/null); then
+  sudo apt-get install --yes curl
+fi
+#make
+if !(hash curl 2>/dev/null); then
+  sudo apt-get install --yes make
+fi
+#python
+if !(hash python 2>/dev/null); then
+  sudo apt-get install --yes python
+fi
 #node
-sudo apt-get install --yes curl
-curl --silent --location https://deb.nodesource.com/setup_0.12 | sudo bash -
-sudo apt-get install --yes nodejs
+if !(hash npm 2>/dev/null); then
+  curl --silent --location https://deb.nodesource.com/setup_0.12 | sudo bash -
+  sudo apt-get install --yes nodejs
+fi
 #homebridge
-npm install homebridge
+if !(hash homebridge 2>/dev/null); then
+  npm install homebridge
+fi
