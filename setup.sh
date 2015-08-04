f467e2c8e1caaf345c5e7dfe40ef159b92701160
penx <penx@users.noreply.github.com>
2015-08-04 18:02:00 +0100

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,6 +1,8 @@
 #prereqs- fresh ubuntu 12.04 image, no ui
-#install Node
+sudo apt-get update
+#node
 sudo apt-get install --yes curl
 curl --silent --location https://deb.nodesource.com/setup_0.12 | sudo bash -
 sudo apt-get install --yes nodejs
+#homebridge
 npm install homebridge
